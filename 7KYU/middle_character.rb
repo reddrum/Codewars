48fa72a168e35186d8e186513291d1e00ac5b2cb
“reddrum” <anton.kranz@me.com>
2018-10-27 23:01:29 +0300

    Get the Middle Character

---
+++ 7KYU/middle_character.rb
@@ -0,0 +1,18 @@
+=begin
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+=end
+
+def get_middle(n)
+  x = (n.length/2) 
+  n.length.even? ? n[x-1..x] : n[x]
+end

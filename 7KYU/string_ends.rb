6f630f4cd37bec64a9c9037b9980a5349392cda4
“reddrum” <anton.kranz@me.com>
2018-10-29 23:23:50 +0300

    String ends with?

---
+++ 7KYU/string_ends.rb
@@ -0,0 +1,12 @@
+=begin
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+=end
+
+def solution(str, ending)
+  str.end_with?(ending)
+end
